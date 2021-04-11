336d0f34a8a522147e7464dc1be851286d29e195
leeyj4805 <leeyj4805@gmail.com>
2021-04-11 21:46:39 +0900

    동적으로 메타데이터 만들기

--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -2,6 +2,8 @@ import styled from "styled-components";
 import cover from "assets/images/title-photo.jpg";
 import { Container, Button } from "components";
 import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet";
+import TitlePhoto from "assets/images/title-photo.jpg";
 
 const Title = styled.h1`
 	font-size: 40px;
@@ -32,19 +34,36 @@ const Text = styled.p`
 	margin-bottom: 24px;
 `;
 
-const Landing = () => (
-	<Container>
-		<Title>나에게 맞는 축구팀은?</Title>
-		<SubTitle>나는 어떤 팀과 잘 맞을까?</SubTitle>
-		<StyledImage src={cover} alt="cover"></StyledImage>
-		<Text>
-			2021년 3월 기준 최고의 축구팀 리버풀, 바이에른 뮌헨, 유벤투스
-			이 중 당신에게 딱 맞는 축구팀은 무엇일까요? 지금 바로 테스트해보세요!
-		</Text>
-		<Link to="/quiz">
-			<Button>테스트 바로가기</Button>
-		</Link>
-	</Container>
-);
+const Landing = () => {
+	const currentUrl = window.location.href;
+	return (
+	<>
+		<Helmet>
+				<meta property="og:url" content={currentUrl} />
+				<meta property="og:title" content="나와 잘 맞는 축구팀은?" />
+				<meta
+                    property="og:description"
+                    content="나와 맞는 축구팀은 어떤 팀일까?"
+                />
+				<meta property="og:image" content={TitlePhoto} />
+				<meta name="twitter:title" content="나와 잘 맞는 축구팀은?" />
+                <meta name="twitter:description" content="나와 맞는 축구팀은 어떤 팀일까?" />
+                <meta name="twitter:image" content={TitlePhoto} />
+		</Helmet>
+		<Container>
+			<Title>나에게 맞는 축구팀은?</Title>
+			<SubTitle>나는 어떤 팀과 잘 맞을까?</SubTitle>
+			<StyledImage src={cover} alt="cover"></StyledImage>
+			<Text>
+				2021년 3월 기준 최고의 축구팀 리버풀, 바이에른 뮌헨, 유벤투스
+				이 중 당신에게 딱 맞는 축구팀은 무엇일까요? 지금 바로 테스트해보세요!
+			</Text>
+			<Link to="/quiz">
+				<Button>테스트 바로가기</Button>
+			</Link>
+		</Container>
+	</>
+	);
+};
 
 export default Landing;
