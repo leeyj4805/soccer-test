1c17d6b87879329981da88f0bf7637f8a298750d
leeyj4805 <leeyj4805@gmail.com>
2021-04-11 21:26:10 +0900

    메타데이터 추가하기

--- src/components/App.js
+++ src/components/App.js
@@ -7,15 +7,27 @@ import { Quiz, Landing, Result, Loading } from "pages";
 import { QUIZZES } from "contents";
 import { Helmet } from "react-helmet";
 import favicon from "assets/images/favicon.ico";
+import Title from "assets/images/title-photo.jpg";
 
 function App() {
 	const [score, setScore] = useState(0);
-	const convertedScore = Math.floor((score / QUIZZES.length) * 100);
+	const convertedScore = Math.floor((score / QUIZZES.length) * 100)
+	const currentUrl = document.location.href;
 
 	return (
 		<ThemeProvider theme={theme}>
 			<GlobalStyle />
 			 <Helmet>
+				<meta property="og:url" content={currentUrl} />
+				<meta property="og:title" content="나와 잘 맞는 축구팀은?" />
+				<meta
+                    property="og:description"
+                    content="나와 맞는 축구팀은 어떤 팀일까?"
+                />
+				<meta property="og:image" content={Title} />
+				<meta name="twitter:title" content="나와 잘 맞는 축구팀은?" />
+                <meta name="twitter:description" content="나와 맞는 축구팀은 어떤 팀일까?" />
+                <meta name="twitter:image" content={Title} />
     			<title>나와 잘 맞는 축구팀은?</title>
 				<link rel="icon" href={favicon} />
             </Helmet>
